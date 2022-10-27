4617101656f3ded765993df61c37c23370f7bf8c
Cydeo-KamranABD <kamran.abdul@icloud.com>
2022-10-27 17:33:05 -0400

    UserStory: TS2-63

---
+++ src/test/java/com/vytrack/tests/US_TS2_63.java
@@ -0,0 +1,118 @@
+package com.vytrack.tests;
+/*
+As a user, I want to learn how to use the pinbar
+
+AC1: when users click click the “Learn how to use this space” link on the homepage, users should see:
+“How To Use Pinbar”  and
+“Use the pin icon on the right top corner of the page to create fast access link in the pinbar.”
+
+TC1: Verify user can see and click the pinbar successfully
+
+1.	Users are on the homepage
+2.	Click the Learn how to use this space message
+3.	Verify the users see 2 messages:
+“How To Use Pinbar”  and
+“Use the pin icon on the right top corner of the page to create fast access link in the pinbar.”
+
+
+AC2: users should see an image on the page.
+[in automation testing,  just verify the image source.]
+Expected source:
+/bundles/oronavigation/images/pinbar-location.jpg
+
+TC2: Verify users can create the pinbar successfully
+
+1.	Users are on the homepage
+2.	Click the Learn how to use this space message
+3.	Verify users see an image
+(manual testing - screenshot proof
+Automation testing - verify image source)
+
+
+
+ */
+
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class US_TS2_63 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void see_TwoMessages_T1 () {
+        driver.get(ConfigurationReader.getProperty("env"));
+
+        // Login: users are on the homepage
+
+        WebElement userName = driver.findElement(By.id("prependedInput"));
+        userName.sendKeys(ConfigurationReader.getProperty("driver1"));
+        WebElement passwordInfo = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordInfo.sendKeys(ConfigurationReader.getProperty("password"));
+        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginBtn.click();
+
+        //Click the Learn how to use this space message
+        WebElement learnLinkMessage = driver.findElement(By.xpath("//div[@class='pin-bar-empty']/a"));
+        BrowserUtils.sleep(3);
+        learnLinkMessage.click();
+
+        //Verify the users see 2 messages:
+        WebElement message1 = driver.findElement(By.xpath("//div[@class='clearfix']//h3"));
+        Assert.assertEquals(message1.getText(), "How To Use Pinbar");
+        WebElement message2 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[2]/div/p[1]"));
+        Assert.assertEquals(message2.getText(), "Use the pin icon on the right top corner of the page to create fast access link in the pinbar.");
+
+        // T1 - BUG FOUND!:
+        // Expected:Use the pin icon on the right top corner of the page to create fast access link in the pinbar.
+        // Actual:Use pin icon on the right top corner of page to create fast access link in the pinbar.
+
+    }
+    @Test
+    public void seeAnImage_T2 () {
+        driver.get(ConfigurationReader.getProperty("env"));
+
+        // Login: users are on the homepage
+        WebElement userName = driver.findElement(By.id("prependedInput"));
+        userName.sendKeys(ConfigurationReader.getProperty("driver1"));
+        WebElement passwordInfo = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordInfo.sendKeys(ConfigurationReader.getProperty("password"));
+        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginBtn.click();
+
+        //Click the Learn how to use this space message
+        WebElement learnLinkMessage = driver.findElement(By.xpath("//div[@class='pin-bar-empty']/a"));
+        BrowserUtils.sleep(3);
+        learnLinkMessage.click();
+
+        //Verify users see an image
+        WebElement image = driver.findElement(By.xpath("//div[@class='container-fluid']//p[2]/img"));
+        String actualImageText = image.getAttribute("src");
+        String expectedImageText = "/bundles/oronavigation/images/pinbar-location.jpg";
+        BrowserUtils.sleep(3);
+        Assert.assertTrue(actualImageText.contains(expectedImageText));
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.quit();
+
+    }
+}
+
---
+++ src/test/java/com/vytrack/tests/US_TS2_64.java
@@ -0,0 +1,115 @@
+package com.vytrack.tests;
+/*
+As a user, I want to access to Vehicle contracts pageAs a user, I want to access to Vehicle contracts page
+
+AC1: Store managers and Sales managers access the Vehicle contracts page.
+Expected URL: https://qa2.vytrack.com/entity/Extend_Entity_VehicleContract
+Expected title: All - Vehicle Contract - Entities - System - Car - Entities - System
+
+1.	Login as store or salesmanager
+2.	Click the Vehicle contracts under the Fleet
+3.	Verify managers can access the Vehicle contracts page
+
+
+AC2: Drivers should NOT able to access the Vehicle contracts page, the app should display “You do not have permission to perform this action.”
+
+1.	Login as drivers
+2.	Click the Vehicle contracts under the Fleet
+3.	Verify users see an error message: “You do not have permission to perform this action.”
+
+
+
+ */
+
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class US_TS2_64 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void managers_access_vehicleContractsPg_T1 () {
+        driver.get(ConfigurationReader.getProperty("env"));
+
+        // Login: users are on the homepage
+        WebElement userName = driver.findElement(By.id("prependedInput"));
+        userName.sendKeys(ConfigurationReader.getProperty("salesManager1"));
+        WebElement passwordInfo = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordInfo.sendKeys(ConfigurationReader.getProperty("password"));
+        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginBtn.click();
+        BrowserUtils.sleep(3);
+        // Click the Vehicle button under the Fleet module
+        WebElement fleetBtn = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]"));
+        fleetBtn.click();
+        BrowserUtils.sleep(3);
+
+        //Click the Vehicle contracts under the Fleet
+        WebElement vehicleContracts = driver.findElement(By.xpath("(//ul[@class='dropdown-menu dropdown-menu-level-1 menu menu-level-1'])[2]/li[6]/a/span"));
+        BrowserUtils.sleep(5);
+        vehicleContracts.click();
+
+        BrowserUtils.sleep(5);
+
+        //Verify managers can access the Vehicle contracts page
+        String actualVcTitlePg = driver.getTitle();
+        BrowserUtils.sleep(3);
+        Assert.assertEquals(actualVcTitlePg, "All - Vehicle Contract - Entities - System - Car - Entities - System");
+    }
+
+    @Test
+    public void users_see_errorMessage_T2 () {
+
+        driver.get(ConfigurationReader.getProperty("env"));
+
+        // Login: users are on the homepage
+        WebElement userName = driver.findElement(By.id("prependedInput"));
+        userName.sendKeys(ConfigurationReader.getProperty("driver1"));
+        WebElement passwordInfo = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordInfo.sendKeys(ConfigurationReader.getProperty("password"));
+        WebElement loginBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginBtn.click();
+        BrowserUtils.sleep(3);
+        // Click the Vehicle button under the Fleet module
+        WebElement fleetBtn = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]"));
+        fleetBtn.click();
+        BrowserUtils.sleep(3);
+
+        //Click the Vehicle contracts under the Fleet
+        WebElement vehicleContracts = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[1]/a/span"));
+        BrowserUtils.sleep(5);
+        vehicleContracts.click();
+
+        BrowserUtils.sleep(5);
+
+        //Verify users see an error message: “You do not have permission to perform this action.”
+        WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"flash-messages\"]/div/div/div[2]/div"));
+
+        String actualMessage = errorMessage.getText();
+        Assert.assertEquals(actualMessage, "You do not have permission to perform this action.");
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.quit();
+    }
+}
